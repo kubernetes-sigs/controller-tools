2d044377061f29f264a0b2e91dd7e1c7e3e5eac3
Phillip Wittrock <pwittroc@google.com>
2018-06-21 14:13:13 -0700

    Scaffold a new Controller for an API

---
+++ pkg/scaffold/controller/controller_test.go
@@ -0,0 +1,35 @@
+package controller_test
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Controller", func() {
+	Describe("scaffolding a controller", func() {
+		Context("for a user defined resource", func() {
+			It("should set the domain in the group to the project domain", func() {
+
+			})
+
+		})
+
+		Context("for a core resource", func() {
+			It("should set the domain in the group to k8s.io for groups with domains", func() {
+
+			})
+
+			It("should not have a domain in the group for groups without domains", func() {
+
+			})
+		})
+
+		It("should return an error if the controller file exists", func() {
+
+		})
+
+		It("should return an error if the controller file exists", func() {
+
+		})
+	})
+
+})
