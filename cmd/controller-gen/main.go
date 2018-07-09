af37d198260e445561923a2874b62c6fda1d468c
Sunil Arora <sunilarora@google.com>
2018-07-09 13:45:05 -0700

    added controller-gen command

--- cmd/controller-gen/main.go
+++ cmd/controller-gen/main.go
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package main
 
 import (
@@ -26,6 +25,35 @@ import (
 )
 
 func main() {
+
+	rootCmd := &cobra.Command{
+		Use:   "controller-gen",
+		Short: "A reference implementation generation tool for Kubernetes APIs.",
+		Long:  `A reference implementation generation tool for Kubernetes APIs.`,
+		Example: `	# Generate RBAC manifests for a project
+	controller-gen rbac
+	
+	# Generate CRD manifests for a project
+	controller-gen crd 
+
+	# Run all the generators for a given project
+	controller-gen all
+`,
+	}
+
+	// add RBAC manifests generator as subcommand
+	rootCmd.AddCommand(
+		newRBACCmd(),
+		newAllSubCmd(),
+	)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func newRBACCmd() *cobra.Command {
 	o := &rbac.ManifestOptions{}
 	cmd := &cobra.Command{
 		Use:   "rbac",
@@ -42,17 +70,25 @@ Usage:
 		},
 	}
 
-	registerFlags(cmd, o)
-
-	if err := cmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-
-func registerFlags(cmd *cobra.Command, o *rbac.ManifestOptions) {
 	f := cmd.Flags()
 	f.StringVar(&o.Name, "name", "manager", "Name to be used as prefix in identifier for manifests")
 	f.StringVar(&o.InputDir, "input-dir", "./pkg", "input directory pointing to Go source files")
 	f.StringVar(&o.OutputDir, "output-dir", "./config", "output directory where generated manifests will be saved.")
+
+	return cmd
+}
+
+func newAllSubCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "all",
+		Short: "runs all generators for a project",
+		Long: `Run all available generators for a given project
+Usage:
+# controller-gen all
+`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO(droot): fill in all the generators
+		},
+	}
+	return cmd
 }
