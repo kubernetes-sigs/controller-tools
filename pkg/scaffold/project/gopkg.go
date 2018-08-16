44105f21983d6491308c6c03d6dae83848ac0f8f
Sunil Arora <sunilarora@google.com>
2018-08-16 16:04:44 -0700

    update gopkg and type test scaffolding for k8s 1.11

--- pkg/scaffold/project/gopkg.go
+++ pkg/scaffold/project/gopkg.go
@@ -148,6 +148,14 @@ name="cloud.google.com/go"
 version="v0.21.0"
 
 [[override]]
+name="github.com/PuerkitoBio/purell"
+version="v1.1.0"
+
+[[override]]
+name="github.com/PuerkitoBio/urlesc"
+revision="de5bf2ad457846296e2031421a34e2568e304e35"
+
+[[override]]
 name="github.com/davecgh/go-spew"
 version="v1.1.0"
 
@@ -166,6 +174,22 @@ revision="9fb12b3b21c5415d16ac18dc5cd42c1cfdd40c4e"
 [[override]]
 name="github.com/go-logr/zapr"
 revision="7536572e8d55209135cd5e7ccf7fce43dca217ab"
+
+[[override]]
+name="github.com/go-openapi/jsonpointer"
+revision="3a0015ad55fa9873f41605d3e8f28cd279c32ab2"
+
+[[override]]
+name="github.com/go-openapi/jsonreference"
+revision="3fb327e6747da3043567ee86abd02bb6376b6be2"
+
+[[override]]
+name="github.com/go-openapi/spec"
+revision="bcff419492eeeb01f76e77d2ebc714dc97b607f5"
+
+[[override]]
+name="github.com/go-openapi/swag"
+revision="811b1089cde9dad18d4d0c2d09fbdbf28dbd27a5"
 
 [[override]]
 name="github.com/gobuffalo/envy"
@@ -188,6 +212,18 @@ name="github.com/golang/protobuf"
 version="v1.1.0"
 
 [[override]]
+name="github.com/google/btree"
+revision="4030bb1f1f0c35b30ca7009e9ebd06849dd45306"
+
+[[override]]
+name="github.com/google/go-github"
+version="v17.0.0"
+
+[[override]]
+name="github.com/google/go-querystring"
+revision="53e6ce116135b80d037921a7fdd5138cf32d7a8a"
+
+[[override]]
 name="github.com/google/gofuzz"
 revision="24818f796faf91cd76ec7bddd72458fbced7a6c1"
 
@@ -196,12 +232,16 @@ name="github.com/googleapis/gnostic"
 version="v0.1.0"
 
 [[override]]
+name="github.com/gregjones/httpcache"
+revision="9cad4c3443a7200dd6400aef47183728de563a38"
+
+[[override]]
 name="github.com/hashicorp/golang-lru"
 revision="0fb14efe8c47ae851c0034ed7a448854d3d34cf3"
 
 [[override]]
-name="github.com/howeyc/gopass"
-revision="bf9dde6d0d2c004a008c27aaee91170c786f6db8"
+name="github.com/hashicorp/hcl"
+revision="ef8a98b0bbce4a65b5aa4c368430a80ddc533168"
 
 [[override]]
 name="github.com/hpcloud/tail"
@@ -209,7 +249,7 @@ version="v1.0.0"
 
 [[override]]
 name="github.com/imdario/mergo"
-version="v0.3.5"
+version="v0.3.6"
 
 [[override]]
 name="github.com/inconshreveable/mousetrap"
@@ -221,11 +261,27 @@ version="v1.2.0"
 
 [[override]]
 name="github.com/json-iterator/go"
-version="1.1.4"
+version="1.1.5"
+
+[[override]]
+name="github.com/magiconair/properties"
+version="v1.8.0"
+
+[[override]]
+name="github.com/mailru/easyjson"
+revision="8b799c424f57fa123fc63a99d6383bc6e4c02578"
 
 [[override]]
 name="github.com/markbates/inflect"
 version="v1.0.0"
+
+[[override]]
+name="github.com/mitchellh/go-homedir"
+revision="58046073cbffe2f25d425fe1331102f55cf719de"
+
+[[override]]
+name="github.com/mitchellh/mapstructure"
+revision="f15292f7a699fcc1a38a80977f80a046874ba8ac"
 
 [[override]]
 name="github.com/modern-go/concurrent"
@@ -244,16 +300,44 @@ name="github.com/onsi/gomega"
 version="v1.4.1"
 
 [[override]]
+name="github.com/pelletier/go-toml"
+version="v1.2.0"
+
+[[override]]
+name="github.com/petar/GoLLRB"
+revision="53be0d36a84c2a886ca057d34b6aa4468df9ccb4"
+
+[[override]]
+name="github.com/peterbourgon/diskv"
+version="v2.0.1"
+
+[[override]]
 name="github.com/pkg/errors"
 version="v0.8.0"
+
+[[override]]
+name="github.com/spf13/afero"
+version="v1.1.1"
+
+[[override]]
+name="github.com/spf13/cast"
+version="v1.2.0"
 
 [[override]]
 name="github.com/spf13/cobra"
 version="v0.0.3"
 
 [[override]]
+name="github.com/spf13/jwalterweatherman"
+revision="14d3d4c518341bea657dd8a226f5121c0ff8c9f2"
+
+[[override]]
 name="github.com/spf13/pflag"
-version="v1.0.1"
+version="v1.0.2"
+
+[[override]]
+name="github.com/spf13/viper"
+version="v1.1.0"
 
 [[override]]
 name="go.uber.org/atomic"
@@ -265,7 +349,7 @@ version="v1.1.0"
 
 [[override]]
 name="go.uber.org/zap"
-version="v1.8.0"
+version="v1.9.1"
 
 [[override]]
 name="golang.org/x/crypto"
@@ -293,7 +377,7 @@ revision="fbb02b2291d28baffd63558aa44b4b56f178d650"
 
 [[override]]
 name="golang.org/x/tools"
-revision="32950ab3be12acf6d472893021373669979907ab"
+revision="1dfe8478fdec02e49c998048ab9f761d64949349"
 
 [[override]]
 name="google.golang.org/appengine"
@@ -313,43 +397,45 @@ version="v2.2.1"
 
 [[override]]
 name="k8s.io/api"
-version="kubernetes-1.10.0"
+version="kubernetes-1.11.0"
 
 [[override]]
 name="k8s.io/apiextensions-apiserver"
-version="kubernetes-1.10.1"
+version="kubernetes-1.11.0"
 
 [[override]]
 name="k8s.io/apimachinery"
-version="kubernetes-1.10.0"
+version="kubernetes-1.11.0"
 
 [[override]]
 name="k8s.io/client-go"
-version="kubernetes-1.10.1"
+version="v8.0.0"
 
 [[override]]
 name="k8s.io/code-generator"
-revision="7b3961072db196a1068e97f76c74ca6bc6b65350"
+revision="caff7734d7c07b6211025f0898aa6168e5bc874d"
 
 [[override]]
 name="k8s.io/gengo"
-revision="906d99f89cd644eecf75ab547b29bf9f876f0b59"
+revision="4242d8e6c5dba56827bb7bcf14ad11cda38f3991"
 
 [[override]]
 name="k8s.io/kube-openapi"
 revision="f08db293d3ef80052d6513ece19792642a289fea"
 
 [[override]]
-name="sigs.k8s.io/controller-runtime"
-branch="master"
-
-[[override]]
-name="sigs.k8s.io/controller-tools"
-branch="master"
-
-[[override]]
 name="sigs.k8s.io/testing_frameworks"
 revision="f53464b8b84b4507805a0b033a8377b225163fea"
+
+[[constraint]]
+name = "sigs.k8s.io/controller-tools"
+branch = "master"
+
+# Using source to test against a branch that has k8s 1.11 release
+[[constraint]]
+name = "sigs.k8s.io/controller-runtime"
+branch = "master"
+source="github.com/n3wscott/controller-runtime"
 
 # For dependency below: Refer to issue https://github.com/golang/dep/issues/1799
 [[override]]
