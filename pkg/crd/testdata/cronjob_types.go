e42c9029f68200fcde0035fed1c4fa89c92acf8b
Haiyan Meng <haiyanmeng@google.com>
2019-07-11 14:57:52 -0700

    Make the `Path` field of the "+kubebuilder:resource" marker optional
    Signed-off-by: Haiyan Meng <haiyanmeng@google.com>

--- pkg/crd/testdata/cronjob_types.go
+++ pkg/crd/testdata/cronjob_types.go
@@ -146,6 +146,7 @@ type CronJobStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:resource
 
 // CronJob is the Schema for the cronjobs API
 type CronJob struct {
