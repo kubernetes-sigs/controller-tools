33d53c11bf281dc198f1aa4bb03502438b3bc01a
Solly Ross <sollyross@google.com>
2019-11-04 19:55:02 -0800

    Update testdata for CRD v1
    This updates the testdata for CRD v1.  All the changes are just
    description wrapping.  We only test generation of CRD v1, since we now
    rely entirely on the k/apiextensions-apiserver conversion logic to do
    the hoisting of the common versions (and have separate tests for that).

---
+++ .run-controller-gen.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This builds and runs controller-gen in a particular context
+# it's the equivalent of `go run sigs.k8s.io/controller-tools/cmd/controller-gen`
+# if you could somehow do that without modifying your go.mod.
+
+current_dir=$(pwd)
+if ! readlink -f . &>/dev/null; then
+    echo "you're probably on OSX.  Please install gnu readlink -- otherwise you're missing the most useful readlink flag."
+    exit 1
+fi
+
+cd $(dirname $(readlink -f ${BASH_SOURCE[0]}))
+go run -v -exec "./.run-in.sh ${current_dir} " ./cmd/controller-gen $@
---
+++ .run-in.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# run something in a directory
+# goes hand-in .run-controller-gen.sh
+
+cd $1
+shift
+$@
