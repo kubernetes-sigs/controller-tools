openshift/kubernetes-sigs-controller-tools

Add x-kubernetes-{embedded-resource,preserve-unknown-fields} support

pkg/crd/markers/validation.go
@@ -22,6 +22,7 @@ import (
 	"encoding/json"
 
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/utils/pointer"
 
 	"sigs.k8s.io/controller-tools/pkg/markers"
 )
@@ -58,6 +59,8 @@ var ValidationMarkers = mustMakeAllWithPrefix("kubebuilder:validation", markers.
 	Enum(nil),
 	Format(""),
 	Type(""),
+	XPreserveUnknownFields{},
+	XEmbeddedResource{},
 )
 
 // FieldOnlyMarkers list field-specific validation markers (i.e. those markers that don't make
@@ -75,6 +78,11 @@ var FieldOnlyMarkers = []*definitionWithHelp{
 
 	must(markers.MakeAnyTypeDefinition("kubebuilder:default", markers.DescribesField, Default{})).
 		WithHelp(Default{}.Help()),
+
+	must(markers.MakeDefinition("kubebuilder:pruning:PreserveUnknownFields", markers.DescribesField, XPreserveUnknownFields{})).
+		WithHelp(XPreserveUnknownFields{}.Help()),
+	must(markers.MakeDefinition("kubebuilder:validation:EmbeddedResource", markers.DescribesField, XEmbeddedResource{})).
+		WithHelp(XEmbeddedResource{}.Help()),
 }
 
 func init() {
@@ -174,6 +182,26 @@ type Nullable struct{}
 type Default struct {
 	Value interface{}
 }
+
+// +controllertools:marker:generateHelp:category="CRD pruning"
+// PreserveUnknownFields stops the apiserver from pruning fields which are not specified.
+//
+// By default the apiserver drops unknown fields from the request payload
+// during the decoding step.stops the API server. This marker stop the API
+// server from doing so. It affects fields recursively, but switches back
+// to normal pruning behaviour if nested  properties or additionalProperties
+// are specified in the schema. This can either be true or undefined. False
+// is forbidden.
+type XPreserveUnknownFields struct{}
+
+// +controllertools:marker:generateHelp:category="CRD validation"
+// EmbeddedResource marks a fields as an embedded resource with apiVersion, kind and metadata fields.
+//
+// An embedded resource is a value that has apiVersion, kind and metadata fields.
+// They are validated implicitly according to the semantics of the currently
+// running apiserver. It is not necessary to add any additional schema for these
+// field, yet it is possible. This can be combined with PreserveUnknownFields.
+type XEmbeddedResource struct{}
 
 func (m Maximum) ApplyToSchema(schema *v1beta1.JSONSchemaProps) error {
 	if schema.Type != "integer" {
@@ -308,5 +336,15 @@ func (m Default) ApplyToSchema(schema *v1beta1.JSONSchemaProps) error {
 		return err
 	}
 	schema.Default = &v1beta1.JSON{Raw: marshalledDefault}
+	return nil
+}
+
+func (m XPreserveUnknownFields) ApplyToSchema(schema *v1beta1.JSONSchemaProps) error {
+	schema.XPreserveUnknownFields = pointer.BoolPtr(true)
+	return nil
+}
+
+func (m XEmbeddedResource) ApplyToSchema(schema *v1beta1.JSONSchemaProps) error {
+	schema.XEmbeddedResource = true
 	return nil
 }

kubernetes-sigs/controller-tools

pkg/crd/markers/validation.go
@@ -58,6 +58,8 @@ var ValidationMarkers = mustMakeAllWithPrefix("kubebuilder:validation", markers.
 	Enum(nil),
 	Format(""),
 	Type(""),
+	XPreserveUnknownFields{},
+	XEmbeddedResource{},
 )
 
 // FieldOnlyMarkers list field-specific validation markers (i.e. those markers that don't make
@@ -75,6 +77,11 @@ var FieldOnlyMarkers = []*definitionWithHelp{
 
 	must(markers.MakeAnyTypeDefinition("kubebuilder:default", markers.DescribesField, Default{})).
 		WithHelp(Default{}.Help()),
+
+	must(markers.MakeDefinition("kubebuilder:pruning:PreserveUnknownFields", markers.DescribesField, XPreserveUnknownFields{})).
+		WithHelp(XPreserveUnknownFields{}.Help()),
+	must(markers.MakeDefinition("kubebuilder:validation:EmbeddedResource", markers.DescribesField, XEmbeddedResource{})).
+		WithHelp(XEmbeddedResource{}.Help()),
 }
 
 func init() {
@@ -174,6 +181,26 @@ type Nullable struct{}
 type Default struct {
 	Value interface{}
 }
+
+// +controllertools:marker:generateHelp:category="CRD processing"
+// PreserveUnknownFields stops the apiserver from pruning fields which are not specified.
+//
+// By default the apiserver drops unknown fields from the request payload
+// during the decoding step. This marker stops the API server from doing so.
+// It affects fields recursively, but switches back to normal pruning behaviour
+// if nested  properties or additionalProperties are specified in the schema.
+// This can either be true or undefined. False
+// is forbidden.
+type XPreserveUnknownFields struct{}
+
+// +controllertools:marker:generateHelp:category="CRD validation"
+// EmbeddedResource marks a fields as an embedded resource with apiVersion, kind and metadata fields.
+//
+// An embedded resource is a value that has apiVersion, kind and metadata fields.
+// They are validated implicitly according to the semantics of the currently
+// running apiserver. It is not necessary to add any additional schema for these
+// field, yet it is possible. This can be combined with PreserveUnknownFields.
+type XEmbeddedResource struct{}
 
 func (m Maximum) ApplyToSchema(schema *apiext.JSONSchemaProps) error {
 	if schema.Type != "integer" {
@@ -308,5 +335,16 @@ func (m Default) ApplyToSchema(schema *apiext.JSONSchemaProps) error {
 		return err
 	}
 	schema.Default = &apiext.JSON{Raw: marshalledDefault}
+	return nil
+}
+
+func (m XPreserveUnknownFields) ApplyToSchema(schema *apiext.JSONSchemaProps) error {
+	defTrue := true
+	schema.XPreserveUnknownFields = &defTrue
+	return nil
+}
+
+func (m XEmbeddedResource) ApplyToSchema(schema *apiext.JSONSchemaProps) error {
+	schema.XEmbeddedResource = true
 	return nil
 }