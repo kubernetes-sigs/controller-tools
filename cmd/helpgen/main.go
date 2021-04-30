02265d54323f8e5ebf8577eeeba42fd5c5845d0f
Solly Ross <sollyross@google.com>
2021-04-29 17:27:12 -0700

    Verify gofmt in test.sh, update helpgen for that
    Updates helpgen to generate gofmt-compliant code, and updates test.sh to
    verify that all code is gofmt-ed.

--- cmd/helpgen/main.go
+++ cmd/helpgen/main.go
@@ -134,7 +134,7 @@ func (g Generator) Generate(ctx *genall.GenerationContext) error {
 
 			for _, field := range info.Fields {
 				summary, details := godocToDetails(field.Name, field.Doc)
-				fmt.Fprintf(outContent, "%[1]q: markers.DetailedHelp{\nSummary: %[2]q,\n Details: %[3]q,\n},\n", field.Name, summary, details)
+				fmt.Fprintf(outContent, "%[1]q: {\nSummary: %[2]q,\n Details: %[3]q,\n},\n", field.Name, summary, details)
 			}
 
 			summary, details := godocToDetails(info.Name, info.Doc)
