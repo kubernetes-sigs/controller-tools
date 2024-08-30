25913ba16c20a1e35a577fd03dc4c70309f035a3
Christian Schlotter <christian.schlotter@broadcom.com>
2024-08-30 09:39:10 +0200

    metrics: implement generator and parser for custom resource configuration and a clusterrole

--- cmd/controller-gen/main.go
+++ cmd/controller-gen/main.go
@@ -32,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-tools/pkg/genall/help"
 	prettyhelp "sigs.k8s.io/controller-tools/pkg/genall/help/pretty"
 	"sigs.k8s.io/controller-tools/pkg/markers"
+	"sigs.k8s.io/controller-tools/pkg/metrics"
 	"sigs.k8s.io/controller-tools/pkg/rbac"
 	"sigs.k8s.io/controller-tools/pkg/schemapatcher"
 	"sigs.k8s.io/controller-tools/pkg/version"
@@ -56,6 +57,7 @@ var (
 		"applyconfiguration": applyconfiguration.Generator{},
 		"webhook":            webhook.Generator{},
 		"schemapatch":        schemapatcher.Generator{},
+		"metrics":            metrics.Generator{},
 	}
 
 	// allOutputRules defines the list of all known output rules, giving
