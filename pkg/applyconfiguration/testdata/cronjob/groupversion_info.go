7d1d17304b002e788ce256ca86ca5412eced7fe5
Joel Speed <joel.speed@hotmail.co.uk>
2025-03-21 14:13:30 +0000

    Update groupversion inline with latest kubebuilder

--- pkg/applyconfiguration/testdata/cronjob/groupversion_info.go
+++ pkg/applyconfiguration/testdata/cronjob/groupversion_info.go
@@ -29,23 +29,11 @@ import (
 var (
 	GroupName     = "testdata.kubebuilder.io"
 	GroupVersion  = schema.GroupVersion{Group: GroupName, Version: "v1"}
-	schemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
-	// Install is a function which adds this version to a scheme
-	Install = schemeBuilder.AddToScheme
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
 
-	// SchemeGroupVersion generated code relies on this name
-	// Deprecated
-	SchemeGroupVersion = GroupVersion
 	// AddToScheme exists solely to keep the old generators creating valid code
-	// DEPRECATED
-	AddToScheme = schemeBuilder.AddToScheme
+	AddToScheme = SchemeBuilder.AddToScheme
 )
-
-// Resource generated code relies on this being here, but it logically belongs to the group
-// DEPRECATED
-func Resource(resource string) schema.GroupResource {
-	return schema.GroupResource{Group: GroupName, Resource: resource}
-}
 
 // Adds the list of known types to api.Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
