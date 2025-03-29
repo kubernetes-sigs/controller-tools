257d6bf0b33005d2e2f2b3625572ff3911684d13
Joel Speed <Joel.speed@hotmail.co.uk>
2025-03-29 22:38:34 +0700

    ✨ Generation of typed apply clients using upstream generator (#818)
    * Stub out generator
    
    * Add apply gen to controller-tools
    
    * Add code-generator to go.mod
    
    * Integrate upstream apply config generator
    
    * Cleanup unused code from previous implementation
    
    * Convert controller-tools tags to genclient
    
    * Add generated clients
    
    * Cleanup markers, ensure correct path for non GOPATH
    
    * Add test to check generated content against generated master
    
    * chore: resolve applyconfig gen correctly
    
    * Migrate to gengo v2
    
    * Update generated test data
    
    * Ignore testdata for the purposes of go generate
    
    * Rename apply command to applyconfiguration
    
    * Rename package to applyconfiguration
    
    * Rework tests to use temporary directory for output files
    
    * Update cronjob_types with the latest version
    
    * Update groupversion inline with latest kubebuilder
    
    * Update go.mod to match latest of top level
    
    * Move test apis into an API v1 folder to be more accurate
    
    * Add test for generating client outside of API directory
    
    * Address feedback on gen.go
    
    ---------
    
    Co-authored-by: Joe Betz <jpbetz@google.com>
    Co-authored-by: Jefftree <jeffrey.ying86@live.com>
    Co-authored-by: Jakob Möller <jmoller@redhat.com>

---
+++ pkg/applyconfiguration/testdata/cronjob/api/v1/groupversion_info.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// +groupName=testdata.kubebuilder.io
+// +versionName=v1
+// +kubebuilder:ac:generate=true
+// +kubebuilder:ac:output:package="applyconfiguration"
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	GroupName     = "testdata.kubebuilder.io"
+	GroupVersion  = schema.GroupVersion{Group: GroupName, Version: "v1"}
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+
+	// AddToScheme exists solely to keep the old generators creating valid code
+	AddToScheme = SchemeBuilder.AddToScheme
+)
+
+// Adds the list of known types to api.Scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	metav1.AddToGroupVersion(scheme, GroupVersion)
+
+	return nil
+}
