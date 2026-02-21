dbcbb281fa6c9360af1783ded80152c5ed864ee5
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2026-02-21 16:46:17 -0500

    Add an integration test for k8s:enum markers

--- pkg/crd/testdata/suite_test.go
+++ pkg/crd/testdata/suite_test.go
@@ -37,7 +37,11 @@ func TestCRD(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	testEnv = &envtest.Environment{}
+	testEnv = &envtest.Environment{
+		CRDInstallOptions: envtest.CRDInstallOptions{
+			Paths: []string{"testdata.kubebuilder.io_enums.yaml"},
+		},
+	}
 
 	cfg, err := testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
